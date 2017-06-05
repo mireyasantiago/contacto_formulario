60f240450faee3bd1599717aaaa53375f60f8cbf
mireyasantiago <mireyasantiago105@gmail.com>
2017-06-05 01:45:08 -0500

    estilo de pagina

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
 
 // IIFE 
 (function () {
-
     var contador = 0;
 
-var cargarPagina = function () {
-	var formulario = $("#formularioContenedor");
-	formulario.submit(contactosNuevos); 
-};
+    var cargarPagina = function () {
+        var formulario = $("#formularioContenedor");
+        formulario.submit(contactosNuevos); 
+        
+       /* var contactosEliminar = $("#contacto");
+        contactosEliminar.click(eliminar);*/
+        
+    };
+
     var contactosNuevos =  function (e) {
 		e.preventDefault();
+
 		// se obtienen los datos
-		var contenedor = $("#contacto");
+        var contenedor = $("#contacto");
+
+        var contenedorContactos = contenedor.val().length;
+		
         // .required = true; si no esta completado el campo no se envia
 		var nombreContenedor = $("#nombre");
 		var nombreContacto = nombreContenedor.val();
 		var telefonoContenedor = $("#telefono");
 		var telefonoContacto = telefonoContenedor.val();
+
 		// se crean los elementos
-		var caja = $("<article/>", {'class': 'card'});
+		var caja = $("<article/>", {'class': 'card blue-grey darken-1'});
 		var parrafo = $("<p/>");
 		var telefono = $("<p/>");
-		
+        var eliminar = $("<input type='button' value='cerrar'/>" ,  { 'class': 'boton__cerrar'});
+
+  
+     
+    
 		parrafo.text(nombreContacto);
 		telefono.text(telefonoContacto);
 
@@ -29,11 +42,22 @@ var cargarPagina = function () {
 		caja.append(parrafo);
 		caja.append(telefono);
 		// Agregarlo a un elemento existente
+        caja.append(eliminar);
+
 		contenedor.prepend(caja);
 		// para borrar el contenido del input
 		nombreContenedor.val("");
 		telefonoContenedor.val("");
 	};
+
+    /*var eliminar = function () {
+    $(this).parent().remove();
+    // para actualizar el contador
+    //cantidadContactos();
+    };*/
+    
+
     $(document).ready(cargarPagina);
-    })();
+
+})();
 
--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $('#modal').modal('open');
+$('#modal').modal('close');
   });
 
-$('#modal1').modal('open');
-$('#modal1').modal('close');
+
